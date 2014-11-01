bf348e4b5301a14958364097ff8c1c51eb10473e
chaochih.lin <dokinkon@gmail.com>
2014-11-01 17:20:44 +0800

    Create iOS Project.

---
+++ iOS/SKCoreKit/SKCoreKit/SKCoreKit.h
@@ -0,0 +1,13 @@
+//
+//  SKCoreKit.h
+//  SKCoreKit
+//
+//  Created by chao-chih lin on 11/1/14.
+//  Copyright (c) 2014 chao-chih lin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKCoreKit : NSObject
+
+@end
---
+++ iOS/SKCoreKit/SKCoreKit/SKCoreKit.m
@@ -0,0 +1,13 @@
+//
+//  SKCoreKit.m
+//  SKCoreKit
+//
+//  Created by chao-chih lin on 11/1/14.
+//  Copyright (c) 2014 chao-chih lin. All rights reserved.
+//
+
+#import "SKCoreKit.h"
+
+@implementation SKCoreKit
+
+@end
